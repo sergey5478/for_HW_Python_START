096a5dc1605e8673e5cda7ad06b4772efcd4df86
sergey <sergey.fomin.90@inbox.ru>
2023-06-11 14:12:39 +0400

    the task_3 for HW #3 was completed, with changes

--- HW_3/task_1.py
+++ HW_3/task_1.py
@@ -3,9 +3,9 @@
 # Вернуть список с дублирующимися элементами.
 # В результирующем списке не должно быть дубликатов.
 list_new = [1, 2, 3, 4, 5, 1, 2, 3]
-res = []
-for value in list_new:
-    if list_new.count(value) > 1 and value not in res:
-        res.append(value)
+res = set()
+for value in set(list_new):
+    if list_new.count(value) > 1:
+        res.add(value)
 
 print(res)
