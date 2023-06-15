a3b5b92125fc046d28fbddb4c10e6b4305a5f64b
sergey <sergey.fomin.90@inbox.ru>
2023-06-15 19:52:04 +0400

    the task_3_txt for HW #4 was completed, with changes

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -2,17 +2,10 @@
 
 
 def transpose_matrix(matrix):
-    """Находим длину и ширину матрицы, меняем местами и создаём пустую."""
-    rows = len(matrix)
-    cols = len(matrix[0])
-
-    transposed_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
-    for i in range(rows):
-        for j in range(cols):
-            transposed_matrix[j][i] = matrix[i][j]
-
+    """Транспонирование матрицы"""
+    transposed_matrix = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
     return transposed_matrix
 
 
-my_matrix = [[1, 2, 3], [4, 5, 6]]
+my_matrix = [[1, 2, 9], [4, 5, 6]]
 print(transpose_matrix(my_matrix))
