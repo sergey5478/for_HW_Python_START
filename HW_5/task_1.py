36e407eb2243e3d5d9b8a997153e228f610bdf72
sergey <sergey.fomin.90@inbox.ru>
2023-06-18 16:04:13 +0400

    the task_1 for HW #5 was completed, with changes

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -6,8 +6,8 @@ import os
 
 def file_path_os(file_path):
     """С помощью os"""
-    name = os.path.basename(file_path)
-    extension = os.path.splitext(name)[1]
+    name, extension = os.path.basename(file_path).split('.')[0], \
+        os.path.basename(file_path).split('.')[1]
     return file_path, name, extension
 
 
@@ -18,7 +18,7 @@ print(file_path_os(MY_FILE_PATH))
 def parse_file_path(file_path):
     """Второй способ"""
     *_, name = file_path.split('\\')
-    return file_path, name, '.' + name.split('.')[1]
+    return file_path, name.split('.')[0], name.split('.')[1]
 
 
 print(parse_file_path(MY_FILE_PATH))
